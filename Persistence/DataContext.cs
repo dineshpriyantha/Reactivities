3cd8cbeab17dbc11347e8e04c8b8063e37a6204d
dineshpriyantha <dinesh.priyantha.uom@gmail.com>
2020-11-05 18:45:57 +0900

    Initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values {get; set;}
+
+        // add hard code value to insert to values in to the db table during migration
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+              new Value{ Id = 1, Name="Values 101"},
+              new Value{ Id = 2, Name = "Value 102"},
+              new Value{ Id = 3, Name = "Value 103"}  
+            );
+        }
+    }
+}
